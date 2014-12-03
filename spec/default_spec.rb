f64e4e1dde411f4c5372eb70ccbbbde36939f4e3
Simon Detheridge <simon@widgit.com>
2014-12-03 12:04:01 +0000

    Fix unit tests for up-to-date chefspec

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,7 +4,7 @@ require_relative 'spec_helper'
 describe "papertrail-rsyslog::default" do
   context 'with the default attributes' do
     before(:all) do
-      @chef_run = ChefSpec::ChefRunner.new(CHEF_RUN_OPTIONS) do |node|
+      @chef_run = ChefSpec::SoloRunner.new(CHEF_RUN_OPTIONS) do |node|
         node.set['papertrail']['port'] = 12345
       end
       @chef_run.converge 'papertrail-rsyslog::default'
@@ -12,38 +12,38 @@ describe "papertrail-rsyslog::default" do
 
     it { @chef_run.should install_package("rsyslog-gnutls") }
     it { @chef_run.should create_remote_file('/etc/syslog.papertrail.crt') }
-    it { @chef_run.should create_file("/etc/rsyslog.d/10-papertrail.conf") }
+    it { @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf") }
 
     context "/etc/rsyslog.d/10-papertrail.conf file" do
       it 'enables TLS' do
-        @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$DefaultNetstreamDriverCAFile /etc/syslog.papertrail.crt")
-        @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionSendStreamDriver gtls")
-        @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionSendStreamDriverMode 1")
-        @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionSendStreamDriverAuthMode x509/name")
+        @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$DefaultNetstreamDriverCAFile /etc/syslog.papertrail.crt")
+        @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionSendStreamDriver gtls")
+        @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionSendStreamDriverMode 1")
+        @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionSendStreamDriverAuthMode x509/name")
       end
 
       it 'should set $ActionResumeRetryCount to "-1"' do
-        @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionResumeRetryCount -1")
+        @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionResumeRetryCount -1")
       end
 
       it 'should set $ActionQueueMaxDiskSpace to "100M"' do
-        @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionQueueMaxDiskSpace 100M")
+        @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionQueueMaxDiskSpace 100M")
       end
 
       it 'should set the forwarding to "*.*     @@logs.papertrailapp.com:12345"' do
-        @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "*.*     @@logs.papertrailapp.com:12345")
+        @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("*.*     @@logs.papertrailapp.com:12345")
       end
     end
 
     context "/etc/rsyslog.d/10-papertrail.conf template" do
-      it { @chef_run.template("/etc/rsyslog.d/10-papertrail.conf").should notify("service[rsyslog]", "restart") }
+      it { @chef_run.template("/etc/rsyslog.d/10-papertrail.conf").should notify "service[rsyslog]" and "restart" }
     end
   end
 
 
   context 'with TLS turned off' do
     before(:all) do
-      @chef_run = ChefSpec::ChefRunner.new(CHEF_RUN_OPTIONS) do |node|
+      @chef_run = ChefSpec::SoloRunner.new(CHEF_RUN_OPTIONS) do |node|
         node.set['papertrail']['port'] = 12345
         node.set['papertrail']['enable_tls'] = false
       end
@@ -52,12 +52,12 @@ describe "papertrail-rsyslog::default" do
 
     it { @chef_run.should_not install_package("rsyslog-gnutls") }
     it { @chef_run.should_not create_remote_file('/etc/syslog.papertrail.crt') }
-    it { @chef_run.should create_file("/etc/rsyslog.d/10-papertrail.conf") }
+    it { @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf") }
 
     context "/etc/rsyslog.d/10-papertrail.conf file" do
       it 'should not enable TLS' do
-        @chef_run.should_not create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionSendStreamDriver gtls")
-        @chef_run.should_not create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionSendStreamDriverMode 1")
+        @chef_run.should_not render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionSendStreamDriver gtls")
+        @chef_run.should_not render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionSendStreamDriverMode 1")
       end
     end
   end
@@ -67,7 +67,7 @@ describe "papertrail-rsyslog::default" do
     context 'and syslog_selector set to cron.*' do
       context 'and queue_disk_space set to 300M' do
         before(:all) do
-          @chef_run = ChefSpec::ChefRunner.new(CHEF_RUN_OPTIONS) do |node|
+          @chef_run = ChefSpec::SoloRunner.new(CHEF_RUN_OPTIONS) do |node|
             node.set['papertrail']['port'] = 12345
             node.set['papertrail']['resume_retry_count'] = 5
             node.set['papertrail']['syslog_selector'] = "cron.*"
@@ -76,19 +76,19 @@ describe "papertrail-rsyslog::default" do
           @chef_run.converge 'papertrail-rsyslog::default'
         end
 
-        it { @chef_run.should create_file("/etc/rsyslog.d/10-papertrail.conf") }
+        it { @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf") }
 
         context "/etc/rsyslog.d/10-papertrail.conf file" do
           it 'should set $ActionResumeRetryCount to "5"' do
-            @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionResumeRetryCount 5")
+            @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionResumeRetryCount 5")
           end
 
           it 'should set $ActionQueueMaxDiskSpace to "300M"' do
-            @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "$ActionQueueMaxDiskSpace 300M")
+            @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("$ActionQueueMaxDiskSpace 300M")
           end
 
           it 'should set the forwarding to "cron.*     @@logs.papertrailapp.com:12345"' do
-            @chef_run.should create_file_with_content( "/etc/rsyslog.d/10-papertrail.conf", "cron.*     @@logs.papertrailapp.com:12345")
+            @chef_run.should render_file("/etc/rsyslog.d/10-papertrail.conf").with_content("cron.*     @@logs.papertrailapp.com:12345")
           end
         end
       end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,12 @@ CHEF_RUN_OPTIONS = {
   :platform => 'ubuntu',
   :version => '12.04'
 }
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
