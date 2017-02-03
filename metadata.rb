adf127ce34816f7dbf164f4ea63cf70ba9ccc277
Remy Loubradou <remyloubradou@gmail.com>
2017-02-03 14:30:51 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "simon@widgit.com"
 license          "Apache 2.0"
 description      "Installs/Configures rsyslog streaming into Papertrail"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.0"
+version          "1.3.0"
 supports 'ubuntu', ">= 12.04"
 
 depends "rsyslog", "~> 5.1.0"
